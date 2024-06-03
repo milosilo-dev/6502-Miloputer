8d489db2d6db1497c700b995085452df598757e0
milosilo-dev <lazerache@gmail.com>
2024-06-03 16:04:34 +0100

    Cleaned up the project, ready to pick it back up!
    I have deleted a lot of stuff, mainly the old logisim files and the source files as they are very schematic dependent.

--- Asm/build.sh
+++ Asm/build.sh
@@ -4,6 +4,7 @@ echo "Finished Assembly..."
 echo "Formating..."
 hexdump -C a.out
 echo "v2.0 raw" > out/a.bin
+hexdump -e a.out
 hexdump -e '16/1 "%02x " "\n"' -n 65000 -v a.out | tr '\r\n' ' ' | sed 's/^\s*\|\s*$//g' >> out/a.bin
 echo "Cleaning up!"
 rm a.out
--- Asm/layout.scm
+++
@@ -1,2 +0,0 @@
-F000 - FFFF is BIOS Rom
-EF00 - EFFF is IO LAtch for terminal / GPU in future
